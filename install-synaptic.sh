02fc48c4a3fcbcb4d2845b4eb4cad9ae0b991ee6
Amos Vryhof <amos@vryhofresearch.com>
2022-05-05 08:54:32 -0400

    Added some WSL Stuff.  Filled in the deadsnakes script

--- install-synaptic.sh
+++ install-synaptic.sh
@@ -1,3 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Install Synaptic and enable the quick filter, which doesn't seem to happen by default anymore
 sudo apt install -y synaptic apt-xapian-index
 sudo update-apt-xapian-index -vf
---
+++ setup-deadsnakes.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sudo add-apt-repository ppa:deadsnakes/ppa && sudo apt update
