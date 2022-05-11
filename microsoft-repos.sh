72a2771acb1bae30543c75d36f0bcedd313c8921
Amos Vryhof <amos@vryhofresearch.com>
2022-05-11 09:51:08 -0400

    Easily add Microsoft repos.

---
+++ microsoft-repos.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+sudo apt install software-properties-common apt-transport-https wget curl ca-certificates gnupg2 ubuntu-keyring -y
+# curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
+wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+sudo apt update
