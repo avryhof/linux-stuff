f44d19737a0510a84380ff98f57015bc93a716fb
Amos Vryhof <avryhof@gmail.com>
2023-03-14 07:42:15 -0400

    Moved wsl scripts to wsl directory

--- wsl-ppa.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-sudo apt install software-properties-common apt-transport-https wget curl ca-certificates gnupg2 ubuntu-keyring -y
-sudo add-apt-repository ppa:mati75/nosnapbrowsers
-sudo add-apt-repository ppa:wslutilities/wslu
-wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-rm packages-microsoft-prod.deb
-sudo apt update && sudo apt upgrade -y packages-microsoft-prod
