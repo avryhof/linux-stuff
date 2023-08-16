bfb2cb80316b45d05315fe3bd3f05f911369f448
Amos Vryhof <amos@vryhofresearch.com>
2023-08-16 11:47:58 -0400

    Better way to add to bashrc/bash_profile

--- install-powerline-linux.sh
+++ install-powerline-linux.sh
@@ -2,10 +2,8 @@
 sudo pip install powerline-shell
 
 if [[ -f "$HOME/.bashrc" ]]; then
-  echo 'function _update_ps1() {\n    PS1=$(powerline-shell $?)\n}' >> $HOME/.bashrc
-  echo 'if [[ $TERM != linux && ! $PROMPT_COMMAND =~ _update_ps1 ]]; then\n    PROMPT_COMMAND='_update_ps1; $PROMPT_COMMAND"\nfi" >> $HOME/.bashrc
+  cat powerline-scripts/bash-linux.txt >> $HOME/.bashrc
 fi
 if [[ -f "$HOME/.bash_profile" ]]; then
-  echo 'function _update_ps1() {\n    PS1=$(powerline-shell $?)\n}' >> $HOME/.bash_profile
-  echo 'if [[ $TERM != linux && ! $PROMPT_COMMAND =~ _update_ps1 ]]; then\n    PROMPT_COMMAND='_update_ps1; $PROMPT_COMMAND"\nfi" >> $HOME/.bash_profile
+  cat powerline-scripts/bash-linux.txt >> $HOME/.bash_profile
 fi
--- install-powerline-mintty.sh
+++ install-powerline-mintty.sh
@@ -3,8 +3,8 @@ mkdir -p $HOME/.bash/themes/git_bash_windows_powerline
 git clone https://github.com/diesire/git_bash_windows_powerline.git $HOME/.bash/themes/git_bash_windows_powerline
 
 if [[ -f "$HOME/.bashrc" ]]; then
-  echo '# Theme\nTHEME=$HOME/.bash/themes/git_bash_windows_powerline/theme.bash\nif [ -f $THEME ]; then\n    . $THEME\nfi\nunset THEME' >> $HOME/.bashrc
+  cat powerline-scripts/bash-mintty.txt >> $HOME/.bashrc
 fi
 if [[ -f "$HOME/.bash_profile" ]]; then
-  echo '# Theme\nTHEME=$HOME/.bash/themes/git_bash_windows_powerline/theme.bash\nif [ -f $THEME ]; then\n    . $THEME\nfi\nunset THEME' >> $HOME/.bash_profile
+  cat powerline-scripts/bash-mintty.txt >> $HOME/.bash_profile
 fi
