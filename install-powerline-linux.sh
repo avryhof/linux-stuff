0c5d8e8549c2e3e20cbe373304fe801eb6cdcd8e
Amos Vryhof <amos@vryhofresearch.com>
2023-08-16 11:40:16 -0400

    Change to single quotes

--- install-powerline-linux.sh
+++ install-powerline-linux.sh
@@ -2,10 +2,10 @@
 sudo pip install powerline-shell
 
 if [[ -f "$HOME/.bashrc" ]]; then
-  echo "function _update_ps1() {\n    PS1=$(powerline-shell $?)\n}" >> $HOME/.bashrc
-  echo "if [[ $TERM != linux && ! $PROMPT_COMMAND =~ _update_ps1 ]]; then\n    PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"\nfi" >> $HOME/.bashrc
+  echo 'function _update_ps1() {\n    PS1=$(powerline-shell $?)\n}' >> $HOME/.bashrc
+  echo 'if [[ $TERM != linux && ! $PROMPT_COMMAND =~ _update_ps1 ]]; then\n    PROMPT_COMMAND='_update_ps1; $PROMPT_COMMAND"\nfi" >> $HOME/.bashrc
 fi
 if [[ -f "$HOME/.bash_profile" ]]; then
-  echo "function _update_ps1() {\n    PS1=$(powerline-shell $?)\n}" >> $HOME/.bash_profile
-  echo "if [[ $TERM != linux && ! $PROMPT_COMMAND =~ _update_ps1 ]]; then\n    PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"\nfi" >> $HOME/.bash_profile
+  echo 'function _update_ps1() {\n    PS1=$(powerline-shell $?)\n}' >> $HOME/.bash_profile
+  echo 'if [[ $TERM != linux && ! $PROMPT_COMMAND =~ _update_ps1 ]]; then\n    PROMPT_COMMAND='_update_ps1; $PROMPT_COMMAND"\nfi" >> $HOME/.bash_profile
 fi
